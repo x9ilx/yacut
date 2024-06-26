59e35f40a52dbb7ba10e224aeeb42118255dfba5
Бондаренко В. В. <AK23041989@Live.com>
2024-06-26 08:24:34 +0600

    change UTC from datetime to utcnow

--- yacut/models.py
+++ yacut/models.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import UTC, datetime
+from datetime import datetime
 
 from flask import request
 
@@ -12,7 +12,7 @@ class URLMap(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     original = db.Column(db.String())
     short = db.Column(db.String(16), unique=True)
-    timestamp = db.Column(db.DateTime, default=datetime.now(UTC))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def to_dict(self):
         return {
